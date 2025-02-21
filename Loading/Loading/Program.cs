6c7af50d65c5d37aa20ebd09465e39b107476f15
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-21 14:52:35 +0900

    practice console by building loading screen

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000); // 1000 = 1 sec
+            Console.Clear(); // remove console
+            Console.WriteLine("■■■■■■■■■■");
+            Console.WriteLine("Loading Complete!");
+        }
+    }
+}
