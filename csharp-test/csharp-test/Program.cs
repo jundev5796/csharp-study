2aefab94cbf56450a85e583889bf134383720c79
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-21 13:07:40 +0900

    create character stat using variables

--- csharp-test/csharp-test/Program.cs
+++ csharp-test/csharp-test/Program.cs
@@ -35,8 +35,33 @@ namespace csharp_test
             //Console.WriteLine(z);
 
             // declare constant variable (fixed variable)
-            const double Pi = 3.14159; // output: 3.14159
-            const int MaxScore = 100; // output: 100
+            //const double Pi = 3.14159; // output: 3.14159
+            //const int MaxScore = 100; // output: 100
+
+            // character stat
+            int atk = 16755;
+            int hp = 78103;
+
+            // character ability
+            int crit = 36;
+            int spec = 1017;
+            int subdue = 41;
+            int agility = 611;
+            int patience = 22;
+            int proficiency = 39;
+
+            Console.WriteLine("기본 특성");
+            Console.WriteLine("공격력: "+atk);
+            Console.WriteLine("최대 생명력: "+hp);
+            Console.WriteLine("--------------------");
+            Console.WriteLine("전투 특성");
+            Console.WriteLine("치명: "+crit);
+            Console.WriteLine("특화: "+spec);
+            Console.WriteLine("제압: "+subdue);
+            Console.WriteLine("신속: "+agility);
+            Console.WriteLine("인내: "+patience);
+            Console.WriteLine("숙련: "+proficiency);
+
         }
     }
 }
