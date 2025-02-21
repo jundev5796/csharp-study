2a9000cd64e39ce2a889f8454aaa640b52375a5d
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-21 13:20:45 +0900

    various number types

---
+++ Study5/Study5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // number type
+            int integerNum = 10; // int
+            float floatNum = 3.14f; // short decmial
+            double doubleNum = 3.14159; // long decimal
+
+            Console.Write(integerNum);
+            Console.Write(floatNum);
+            Console.Write(doubleNum);
+        }
+    }
+}
