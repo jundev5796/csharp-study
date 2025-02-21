3ebf8824b452ce146eea842253631e2b31990c93
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-21 09:57:14 +0900

    create c# test for study

---
+++ csharp-test/csharp-test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // comment
+            Console.WriteLine("Connect Github");
+        }
+    }
+}
