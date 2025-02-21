35f764e1ed59a286eb20d0e9e0cf80351e7747c7
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-21 15:15:17 +0900

    console.readline

---
+++ studyReadLine/studyReadLine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get user input w/ string
+            Console.Write("Write your name: ");
+            string userName = Console.ReadLine(); // get input from user
+
+            Console.WriteLine($"Hello, {userName}!");
+
+            // change string to int
+            Console.Write("Write your age: ");
+            string input = Console.ReadLine(); // get input from user
+            int age = int.Parse(input); // string to int
+
+            Console.WriteLine($"You will be {age + 1} next year!"); // output
+        }
+    }
+}
