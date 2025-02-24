6f8b18b4c84a778dd286805de7c684f8e96924cb
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-24 17:32:48 +0900

    day3-problem4

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Schema;
 
 namespace Assignment2
 {
@@ -50,58 +51,90 @@ namespace Assignment2
 
             /////////////////////////////////////////////////////////////////////
 
-            // Problem #3
-            Console.Write("Your Money: ");
-            string character = "Lion";
-            int money = int.Parse(Console.ReadLine());
-            string weapon = default;
-            int atk = 0;
-            Console.Write("\n");
+            //// Problem #3
+            //Console.Write("Your Money: ");
+            //string character = "Lion";
+            //int money = int.Parse(Console.ReadLine());
+            //string weapon = default;
+            //int atk = 0;
+            //Console.Write("\n");
 
-            if (money >= 0 && money <= 100)
+            //if (money >= 0 && money <= 100)
+            //{
+            //    weapon = "무한의 대검";
+            //    atk += 1;
+            //}
+            //else if (money >= 101 && money <= 200)
+            //{
+            //    weapon = "카타나";
+            //    atk += 2;
+            //}
+            //else if (money >= 201 && money <= 300)
+            //{
+            //    weapon = "진은검";
+            //    atk += 3;
+            //}
+            //else if (money >= 301 && money <= 400)
+            //{
+            //    weapon = "집판검";
+            //    atk += 4;
+            //}
+            //else if (money >= 401 && money <= 500)
+            //{
+            //    weapon = "엑스칼리버";
+            //    atk += 5;
+            //}
+            //else if (money >= 501 && money <= 600)
+            //{
+            //    weapon = "유령검";
+            //    atk += 6;
+            //}
+            //else if (money > 600)
+            //{
+            //    weapon = "전설의 검";
+            //    atk += 7;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No more choices!");
+            //}
+
+            //string final_weapon = weapon + " " + "+" + atk;
+
+            //Console.WriteLine($"Character: {character}");
+            //Console.WriteLine($"Weapon: {final_weapon}");
+
+            // Problem #4
+            int a, b, c;
+            int max_value = 0;
+
+            Console.Write("1st Number: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("2nd Number: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("3rd Number: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
             {
-                weapon = "무한의 대검";
-                atk += 1;
+                max_value = a;
             }
-            else if (money >= 101 && money <= 200)
+            else if (b > a && b > c)
             {
-                weapon = "카타나";
-                atk += 2;
+                max_value = b;
             }
-            else if (money >= 201 && money <= 300)
+            else if (c > a && c > b)
             {
-                weapon = "진은검";
-                atk += 3;
-            }
-            else if (money >= 301 && money <= 400)
-            {
-                weapon = "집판검";
-                atk += 4;
-            }
-            else if (money >= 401 && money <= 500)
-            {
-                weapon = "엑스칼리버";
-                atk += 5;
-            }
-            else if (money >= 501 && money <= 600)
-            {
-                weapon = "유령검";
-                atk += 6;
-            }
-            else if (money > 600)
-            {
-                weapon = "전설의 검";
-                atk += 7;
+                max_value = c;
             }
             else
             {
-                Console.WriteLine("No more choices!");
+                Console.WriteLine("Invalid Input");
             }
 
-            string final_weapon = weapon + " " + "+" + atk;
-
-            Console.WriteLine($"Character: {character}");
-            Console.WriteLine($"Weapon: {final_weapon}");
+            Console.WriteLine($"Max Value: " + max_value);
         }
     }
 }
