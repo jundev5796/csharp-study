c32a4b31065d5d9b885de9ba4ee344c51af9682d
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-25 10:29:21 +0900

    day4-problem2

--- day4-assignment/day4-assignment/Program.cs
+++ day4-assignment/day4-assignment/Program.cs
@@ -10,35 +10,41 @@ namespace day4_assignment
     {
         static void Main(string[] args)
         {
-            // problem #1
+            //// Problem #1
 
-            // Choose a job
-            // 1. Warrior
-            // 2. Mage
-            // 3. Thief
+            //// Choose a job
+            //// 1. Warrior
+            //// 2. Mage
+            //// 3. Thief
 
-            Console.WriteLine("Choose a Job (1. Warrior 2. Mage 3. Thief: ");
-            int job = int.Parse(Console.ReadLine());
-            switch (job)
+            //Console.WriteLine("Choose a Job (1. Warrior 2. Mage 3. Thief: ");
+            //int job = int.Parse(Console.ReadLine());
+            //switch (job)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Warrior");
+            //        Console.WriteLine("ATK: 100");
+            //        Console.WriteLine("DEF: 90");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Mager");
+            //        Console.WriteLine("ATK: 110");
+            //        Console.WriteLine("DEF: 80");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Thief");
+            //        Console.WriteLine("ATK: 115");
+            //        Console.WriteLine("DEF: 70");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid Day");
+            //        break;
+            //}
+
+            // Problem #2
+            for (int i = 0; i <= 9; i++)
             {
-                case 1:
-                    Console.WriteLine("Warrior");
-                    Console.WriteLine("ATK: 100");
-                    Console.WriteLine("DEF: 90");
-                    break;
-                case 2:
-                    Console.WriteLine("Mager");
-                    Console.WriteLine("ATK: 110");
-                    Console.WriteLine("DEF: 80");
-                    break;
-                case 3:
-                    Console.WriteLine("Thief");
-                    Console.WriteLine("ATK: 115");
-                    Console.WriteLine("DEF: 70");
-                    break;
-                default:
-                    Console.WriteLine("Invalid Day");
-                    break;
+                Console.WriteLine($"Number: {i}");
             }
         }
     }
