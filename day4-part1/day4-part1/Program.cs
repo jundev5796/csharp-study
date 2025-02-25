56d93a12d63c3450f95dc44e9ca0b2281c242571
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-25 10:19:32 +0900

    for loop

--- day4-part1/day4-part1/Program.cs
+++ day4-part1/day4-part1/Program.cs
@@ -10,35 +10,42 @@ namespace day4_part1
     {
         static void Main(string[] args)
         {
-            // switch
-            int day = 3;
-            switch (day)
+            //// switch
+            //int day = 3;
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Saturday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid Day");
+            //        break;
+            //}
+
+            // for loop
+            for (int i = 1; i <= 5; i++)
             {
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednesday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thursday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
-                case 7:
-                    Console.WriteLine("Sunday");
-                    break;
-                default:
-                    Console.WriteLine("Invalid Day");
-                    break;
+                Console.WriteLine($"Number: {i}");
             }
         }
     }
 }
+
