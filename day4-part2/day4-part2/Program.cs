dd019cc66f8d5cc18c6848b08a50c7a63a71a97e
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-25 14:04:55 +0900

    day4-project1

---
+++ day4-part2/day4-part2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace day4_대장장이키우기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // random
+            Random rand = new Random();
+
+            Console.WriteLine(" 대장장이 키우기 ");
+
+            int pmoney = 100;
+            int input;
+            int rnd;
+
+            Thread.Sleep(500);
+
+            // infinite loop
+            while (true)
+            {
+                Console.Clear(); // Erase Screen
+                Console.WriteLine("1. 나무캐기 ");
+                Console.WriteLine("2. 장비뽑기 ");
+                Console.WriteLine("3. 나가기 ");
+                Console.Write("입력 : ");
+                input = int.Parse(Console.ReadLine()); // input에 키로 눌린 숫자 담기
+
+                if (input == 1)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("나무캐기(엔터)");
+                        Console.WriteLine("뒤로가기 x");
+
+                        string str = Console.ReadLine();
+
+                        pmoney += 100;
+                        Console.WriteLine("소지금 : " + pmoney);
+                        if (str == "x")
+                        {
+                            Console.WriteLine("뒤로가기");
+                            break;
+                        }
+                    }
+                }
+                else if (input == 2)
+                {
+                    // 장비뽑기
+                    if (pmoney >= 1000)
+                    {
+                        pmoney -= 1000;
+
+                        for (int i = 1; i <= 20; i++)
+                        {
+                            rnd = rand.Next(1, 101);
+
+                            if (rnd == 1)
+                            {
+                                Console.WriteLine("도끼등급 SSS");
+                            }
+                            else if (rnd >= 2 && rnd <= 6)
+                            {
+                                Console.WriteLine("도끼등급 SS");
+                            }
+                            else if (rnd >= 7 && rnd <= 17)
+                            {
+                                Console.WriteLine("도끼등급 S");
+                            }
+                            else if (rnd >= 18 && rnd <= 38)
+                            {
+                                Console.WriteLine("도끼등급 A");
+                            }
+                            else if (rnd >= 39 && rnd <= 69)
+                            {
+                                Console.WriteLine("도끼등급 B");
+                            }
+                            else
+                            {
+                                Console.WriteLine("도끼등급 C");
+                            }
+                            Thread.Sleep(500); // 0.5초정도로 뽑혀라.
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("돈이 부족합니다. \n");
+                        Thread.Sleep(1000);
+                    }
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine("나갑니다.");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
