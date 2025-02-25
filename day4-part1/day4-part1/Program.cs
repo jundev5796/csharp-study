9823ea46bdcf075281e5fdd310fd37edf6680f3f
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-25 09:27:06 +0900

    switch conditional

---
+++ day4-part1/day4-part1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day4_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // switch
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Day");
+                    break;
+            }
+        }
+    }
+}
