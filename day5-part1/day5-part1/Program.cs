ce3fd57a6679bf5e40434fb05cfeba1fad3bd617
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-26 09:34:37 +0900

    declaring array

--- day5-part1/day5-part1/Program.cs
+++ day5-part1/day5-part1/Program.cs
@@ -10,18 +10,28 @@ namespace day5_part1
     {
         static void Main(string[] args)
         {
-            // #1 Array
+            //// #1 Array
 
-            // array space starts from 0 not 1
-            int[] num = new int[3]; // create 3 memory (index: 0~2)
+            //// array space starts from 0 not 1
+            //int[] num = new int[3]; // create 3 memory (index: 0~2)
 
-            num[0] = 10;
-            num[1] = 20;
-            num[2] = 30;
+            //num[0] = 10;
+            //num[1] = 20;
+            //num[2] = 30;
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(num[i]);
+            //}
+
+            // declaring array
+            int[] numbers = { 1, 2, 3 }; // simple way of setting size and value
+            int[] numbers1 = new int[3]; // set size only
+            int[] numbers2 = new int[] { 1, 2, 3 }; // set both size and value
 
             for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(num[i]);
+                Console.WriteLine(numbers[i]);
             }
         }
     }
