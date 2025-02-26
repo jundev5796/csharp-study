434a85e1a434a81056ab752cfdbc0f6ebf0c5abd
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-26 15:34:19 +0900

    function + function parameters

---
+++ day5-part2/day5-part2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace day5_part2
+{
+    class Program
+    {
+        // #2 Function
+
+        // memory
+        // - stack (provides data) (ex. int, string)
+        // - heap (manual input of data) (ex. new)
+        // - static memory
+
+        // Phase 1
+        static void Loading()
+        {
+            Console.WriteLine("로딩중.");
+            Thread.Sleep(1000);
+            Console.WriteLine("로딩중..");
+            Thread.Sleep(1000);
+            Console.WriteLine("로딩중...");
+            Thread.Sleep(1000);
+        }
+
+        // Phase 2
+        static void AttackFunction(int _Attack)
+        {
+            Console.WriteLine("ATK: " + _Attack);
+        }
+
+        static void Main(string[] args)
+        {
+            int atk = 0;
+            Console.WriteLine("Enter Character ATK: ");
+            atk = int.Parse(Console.ReadLine());
+
+            AttackFunction(atk);
+        }
+    }
+}
