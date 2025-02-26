6b71096f239a3dca0c4b7f5c56fdb5a3c222c7c4
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-26 14:35:38 +0900

    day5-project-bingo(2)

---
+++ day5-project-bingo2/day5-project-bingo2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day5_project_bingo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[5, 5]; // 5x5 bingo board
+            bool[,] marked = new bool[5, 5]; // check chosen number
+
+            int bingoCount = 0;
+
+            Random random = new Random();
+
+            //빙고판 초기화
+            int[] numbers = new int[25];
+
+            for (int i = 0; i < 25; i++)
+                numbers[i] = i + 1;
+
+            //랜덤 섞기(Fisher-Yates Shuffle)
+            for (int i = 0; i < 100; i++)
+            {
+                int a = random.Next(25);
+                int b = random.Next(25);
+
+                // C#의 튜플(Tuple) 문법을 활용하여 두 변수의 값을 교환(Swap)하는 방법
+                (numbers[a], numbers[b]) = (numbers[b], numbers[a]);
+
+            }
+
+
+            //2차원 배열로 변환
+            int index = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    board[i, j] = numbers[index++];
+                }
+            }
+
+            //게임시작
+            while (bingoCount < 5)
+            {
+                Console.Clear();
+
+                //빙고판 출력
+                Console.WriteLine("현재 빙고판");
+
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (marked[i, j])
+                            Console.Write(" X ");
+                        else
+                            Console.Write($"{board[i, j],2} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine($"현재 빙고 개수 : {bingoCount}");
+                Console.Write("숫자를 입력하세요 (1~25) : ");
+                int number = int.Parse(Console.ReadLine());
+
+                bool found = false;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (board[i, j] == number)
+                        {
+                            marked[i, j] = true; //숫자를 X로 변경
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found)
+                        break;
+                }
+
+                //빙고 개수 체크
+                bingoCount = 0;
+
+                //가로체크
+                for (int i = 0; i < 5; i++)
+                {
+                    bool rowBingo = true;
+                    for (int j = 0; j < 5; j++)
+                        if (!marked[i, j]) rowBingo = false;
+
+                    if (rowBingo) bingoCount++;
+                }
+
+
+                //세로체크
+                for (int j = 0; j < 5; j++)
+                {
+                    bool colBingo = true;
+
+                    for (int i = 0; i < 5; i++)
+                        if (!marked[i, j]) colBingo = false;
+
+                    if (colBingo) bingoCount++;
+                }
+
+
+                //대각선 체크 (왼쪽위 ->오른쪽 아래)
+
+                bool diag1Bingo = true;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (!marked[i, i])
+                        diag1Bingo = false;
+                }
+                if (diag1Bingo)
+                    bingoCount++;
+
+
+                //대각선 오른쪽위 ->왼쪽아래
+                bool diag2Bingo = true;
+
+                for (int i = 0; i < 5; i++)
+                    if (!marked[i, 4 - i]) diag2Bingo = false;
+
+
+                if (diag2Bingo) bingoCount++;
+
+            }
+
+            Console.WriteLine("빙고 5개 완성! 게임종료");
+        }
+    }
+}
