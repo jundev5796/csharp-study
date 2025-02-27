0349d234696c88cdf4d52c48da80981da891b5d6
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 13:30:21 +0900

    [day6] struct (more example)

---
+++ day6-part7/day6-part7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6_part7
+{
+    struct Rectangle
+    {
+        public int Width;
+        public int Height;
+
+        public int GetArea() => Width * Height;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rect = new Rectangle { Width = 5, Height = 4 };
+
+            Console.WriteLine($"Area: {rect.GetArea()}");
+        }
+    }
+}
