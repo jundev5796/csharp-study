adf3eec0995888f8937e2bad8365bf91258c74ac
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 09:20:29 +0900

    function (basic)

---
+++ day6-part1/day6-part1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6_part1
+{
+    class Program
+    {
+        // 1. Function (no parameter, no return value)
+        static void PrintHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintHello(); // output
+        }
+    }
+}
