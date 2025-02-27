c8fae6fb74ea92b545d39f80e2dfdd0c711a093c
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 11:10:10 +0900

    [day6] arrow function

---
+++ day6-part4/day6-part4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6_part4
+{
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // arrow function
+        static int AddArrow(int a, int b) => a + b;
+
+        static void Main(string[] args)
+        {
+            // C# Arrow Function (=>)
+            Console.WriteLine(Add(3, 5));
+            Console.WriteLine(AddArrow(3, 5));
+        }
+    }
+}
