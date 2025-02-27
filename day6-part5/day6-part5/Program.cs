7fd601e5c2d6d43cfc3f9bcf743506be77feb441
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 11:23:26 +0900

    [day6] math classes

---
+++ day6-part5/day6-part5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6_part5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Math Class
+            Console.WriteLine($"Pi: {Math.PI}");
+            Console.WriteLine($"Squre root of 25: {Math.Sqrt(25)}");
+            Console.WriteLine($"Power (2^3): {Math.Pow(2, 3)}");
+            Console.WriteLine($"Round(3.75): {Math.Round(3.75)}");
+        }
+    }
+}
