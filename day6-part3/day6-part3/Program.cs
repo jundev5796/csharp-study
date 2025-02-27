88579d72683b9a77142ff6eb3b6c837abc8c6821
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 10:46:33 +0900

    [day6] namespace

---
+++ day6-part3/day6-part3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// namespace (avoid clash between other variables and functions)
+namespace dev1
+{
+    class MyClass
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
+
+namespace day6_part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dev1.MyClass.SayHello();
+        }
+    }
+}
