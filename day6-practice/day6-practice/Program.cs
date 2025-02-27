8180cd84bfa5ee2cffda2cf1f7987298789a2d46
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 11:50:49 +0900

    [day6] practice problem #1

---
+++ day6-practice/day6-practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6_practice
+{
+    class Program
+    {
+        // Problem #1
+        enum WeaponType
+        {
+            Sword,
+            Bow,
+            Staff
+        }
+
+        static void ChooseWeapon(WeaponType weapon)
+        {
+            if (weapon == WeaponType.Sword)
+            {
+                Console.WriteLine("You chose a Sword!");
+            }
+            else if (weapon == WeaponType.Bow)
+            {
+                Console.WriteLine("You chose a Bow!");
+            }
+            else if (weapon == WeaponType.Staff)
+            {
+                Console.WriteLine("You chose a Staff!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // 1.
+            ChooseWeapon(WeaponType.Staff);
+        }
+    }
+}
