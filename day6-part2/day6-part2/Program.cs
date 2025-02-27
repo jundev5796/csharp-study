70592451fac99ca8534f99bff97817a62958e167
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 10:32:15 +0900

    recursive function

--- day6-part2/day6-part2/Program.cs
+++ day6-part2/day6-part2/Program.cs
@@ -10,13 +10,16 @@ namespace day6_part2
     {
         static void Main(string[] args)
         {
-            // params
+            // 1.
             Console.WriteLine(Sum(1, 2, 3));
 
             Console.WriteLine(Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+            // 2.
+            Console.WriteLine(Factorial(5)); // Factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
         }
 
-        // params
+        // 1. params
         static int Sum(params int[] numbers)
         {
             int total = 0;
@@ -27,6 +30,15 @@ namespace day6_part2
             }
 
             return total;
+        }
+
+        // 2. recursive function (function that calls itself)
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            return n * Factorial(n - 1);
         }
     }
 }
