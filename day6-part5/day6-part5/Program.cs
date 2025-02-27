c976e32dd1c1b8440020010f10b1afcb9f9ac89b
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 11:36:24 +0900

    [day6] enum

--- day6-part5/day6-part5/Program.cs
+++ day6-part5/day6-part5/Program.cs
@@ -8,13 +8,46 @@ namespace day6_part5
 {
     class Program
     {
+        // 1. Enumeration
+        enum DayOfWeek
+        {
+            Sunday, // 0
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        // 2. Enum (value change)
+        enum StatusCode
+        {
+            Success = 200,
+            BadRequest = 400,
+            Unauthorized = 401,
+            NotFound = 404
+        }
+
         static void Main(string[] args)
         {
-            // Math Class
-            Console.WriteLine($"Pi: {Math.PI}");
-            Console.WriteLine($"Squre root of 25: {Math.Sqrt(25)}");
-            Console.WriteLine($"Power (2^3): {Math.Pow(2, 3)}");
-            Console.WriteLine($"Round(3.75): {Math.Round(3.75)}");
+            // 1.
+            DayOfWeek today = DayOfWeek.Wednesday;
+
+            Console.WriteLine(today);
+            Console.WriteLine((int)today);
+
+            // 2.
+            StatusCode status = StatusCode.NotFound;
+
+            Console.WriteLine(status);
+            Console.WriteLine((int)status);
+
+            //// Math Class
+            //Console.WriteLine($"Pi: {Math.PI}");
+            //Console.WriteLine($"Squre root of 25: {Math.Sqrt(25)}");
+            //Console.WriteLine($"Power (2^3): {Math.Pow(2, 3)}");
+            //Console.WriteLine($"Round(3.75): {Math.Round(3.75)}");
         }
     }
 }
