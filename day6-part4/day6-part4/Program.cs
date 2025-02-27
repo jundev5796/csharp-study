cbdbc12112a9432db7818a274230a0e185a98aa0
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-27 11:13:49 +0900

    [day6] arrow function w/ string

--- day6-part4/day6-part4/Program.cs
+++ day6-part4/day6-part4/Program.cs
@@ -8,6 +8,7 @@ namespace day6_part4
 {
     class Program
     {
+        // basic function
         static int Add(int a, int b)
         {
             return a + b;
@@ -16,11 +17,23 @@ namespace day6_part4
         // arrow function
         static int AddArrow(int a, int b) => a + b;
 
+        // basic function
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        // arrow function
+        static void PrintMessageArrow() => Console.WriteLine("Hi");
+
         static void Main(string[] args)
         {
             // C# Arrow Function (=>)
             Console.WriteLine(Add(3, 5));
             Console.WriteLine(AddArrow(3, 5));
+
+            PrintMessage();
+            PrintMessageArrow();
         }
     }
 }
