a839424d84c8c721f8aa6e1b4ff9ede35b4087cd
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-28 11:28:46 +0900

    [day7] class - get/set

---
+++ day7-part3/day7-part3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7_part3
+{
+    // 3. get/set type function
+    class Person
+    {
+        private string name; // protected variable
+
+        // set value (setter)
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+
+        // get value (getter)
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C#에서 get/set 방식의 함수와 프로퍼티 비교
+            //C#에서는 객체의 값을 읽고(get), 설정(set)하는
+            //방식으로 함수(get/set 메서드) 또는
+            //**프로퍼티(Property)**를 사용할 수 있습니다.
+            Person p = new Person();
+
+            p.SetName("Tom");
+
+            Console.WriteLine("Name: " + p.GetName());
+        }
+    }
+}
