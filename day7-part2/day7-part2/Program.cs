eda5e6d29c3ff17e3beb05f4a7a160101f54023b
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-28 09:43:48 +0900

    [day7] class constructors

--- day7-part2/day7-part2/Program.cs
+++ day7-part2/day7-part2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,23 @@ namespace day7_part2
     // 2. Class
     class Person
     {
-        public string name;
+        public string Name;
+        public int Age;
+
+        // constructor
+        // - A special method that is automatically executed when a class is instantiated as an object.
+        // - It has the same name as the class and does not have a return type(not even void).
+        // - It is often used to set initial values when creating an object.
+        public Person()
+        {
+            Name = "No Name";
+            Age = 0;
+            Console.WriteLine("constructor activates");
+        }
 
         public void ShowInfo()
         {
-            Console.WriteLine("Name: " + name);
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
         }
     }
 
