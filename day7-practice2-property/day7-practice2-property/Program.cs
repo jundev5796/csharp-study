832ae304c346b2bd11910c39b0a91b7638d3a052
hyunjunbaek <hjbaek5796@gmail.com>
2025-02-28 11:55:25 +0900

    [day7] practice2-property

---
+++ day7-practice2-property/day7-practice2-property/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7_practice2_property
+{
+    class Marine
+    {
+        public string Name { get; private set; } = "Marine";
+        public int Mineral { get; set; } = 100;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Marine m = new Marine();
+
+            Console.WriteLine($"Name: {m.Name}, Mineral: {m.Mineral}");
+        }
+    }
+}
---
+++ day7-practice2-property/day7-practice2-property/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("day7-practice2-property")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("day7-practice2-property")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ce3e872-af6e-4835-a249-9e3a0a718496")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
