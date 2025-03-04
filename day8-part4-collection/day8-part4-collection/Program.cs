1d2dfc2a72a6e65071d20f0ac182b61fe6e9c86e
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 14:44:01 +0900

    [day8-part4] stack, list examples

--- day8-part4-collection/day8-part4-collection/Program.cs
+++ day8-part4-collection/day8-part4-collection/Program.cs
@@ -16,11 +16,32 @@ namespace day8_part4_collection
 
         static void Main(string[] args)
         {
-            Cup<string> cupOfString = new Cup<string> { Content = "Coffee" };
-            Cup<int> cupOfInt = new Cup<int> { Content = 42 };
+            //Cup<string> cupOfString = new Cup<string> { Content = "Coffee" };
+            //Cup<int> cupOfInt = new Cup<int> { Content = 42 };
 
-            Console.WriteLine($"CupOfString: {cupOfString.Content}");
-            Console.WriteLine($"cupOfInt: {cupOfInt.Content}");
+            //Console.WriteLine($"CupOfString: {cupOfString.Content}");
+            //Console.WriteLine($"cupOfInt: {cupOfInt.Content}");
+
+            // w/ stack
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            // w/ list
+            List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
+            names.Add("Dave");
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
