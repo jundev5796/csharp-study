eb30f792ff81a808adec4f331757e6f250490596
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 10:22:45 +0900

    [day8-part2] value/ref types

---
+++ day8-part2/day8-part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day8_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // value/ref types
+            int valueType = 10;
+            object referenceType = valueType; // specifically refers to the "valueType" variable that appears before itself
+
+            valueType = 20;
+
+            Console.WriteLine($"ValueType: {valueType}"); // 20
+            Console.WriteLine($"ReferenceType: {referenceType}"); // 19
+        }
+    }
+}
