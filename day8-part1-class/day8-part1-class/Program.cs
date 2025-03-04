9201a42fcb1c684c3f14b17436d844fcc8948a47
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 09:26:06 +0900

    [day8] random class

--- day8-part1-class/day8-part1-class/Program.cs
+++ day8-part1-class/day8-part1-class/Program.cs
@@ -10,13 +10,19 @@ namespace day8_part1_class
     {
         static void Main(string[] args)
         {
-            // 1. Environment
-            Console.WriteLine("Terminate Program");
+            //// 1. Environment
+            //Console.WriteLine("Terminate Program");
 
-            string path = Environment.GetEnvironmentVariable("PATH");
-            Console.WriteLine($"PATH: {path}");
+            //string path = Environment.GetEnvironmentVariable("PATH");
+            //Console.WriteLine($"PATH: {path}");
 
-            Environment.Exit(0); // program ends automatically
+            //Environment.Exit(0); // program ends automatically
+
+            // 2. Random
+            Random random = new Random();
+
+            int randomNumber = random.Next(1, 101);
+            Console.WriteLine("Random Number: " + randomNumber);
         }
     }
 }
