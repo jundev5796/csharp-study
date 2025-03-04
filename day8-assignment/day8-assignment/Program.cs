89482202c1d54d96ffdab9b7342f5b9f13007ebf
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 16:27:40 +0900

    [day8-assignment] problem #2

--- day8-assignment/day8-assignment/Program.cs
+++ day8-assignment/day8-assignment/Program.cs
@@ -8,27 +8,39 @@ namespace day8_assignment
 {
     class Program
     {
-        // Problem #1
-        class Warrior
-        {
-            public string Name { get; set; }
-            public int Score { get; set; }
-            public int Strength { get; set; }
+        //// Problem #1
+        //class Warrior
+        //{
+        //    public string Name { get; set; }
+        //    public int Score { get; set; }
+        //    public int Strength { get; set; }
 
-            public void Stat()
-            {
-                Console.WriteLine($"Name: {Name}, Score: {Score}, Strength: {Strength}");
-            }
-        }
+        //    public void Stat()
+        //    {
+        //        Console.WriteLine($"Name: {Name}, Score: {Score}, Strength: {Strength}");
+        //    }
+        //}
 
         static void Main(string[] args)
         {
-            // 1.
-            Warrior warrior = new Warrior();
-            warrior.Name = "Lion";
-            warrior.Score = 85;
-            warrior.Strength = 98;
-            warrior.Stat();
+            //// 1.
+            //Warrior warrior = new Warrior();
+            //warrior.Name = "Lion";
+            //warrior.Score = 85;
+            //warrior.Strength = 98;
+            //warrior.Stat();
+
+            // Problem #2
+            try
+            {
+                Console.Write("정수를 입력하세요: ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine($"입력결과: {num}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("올바른 숫자를 입력하세요!");
+            }
         }
     }
 }
