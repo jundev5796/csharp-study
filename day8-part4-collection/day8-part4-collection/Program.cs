c736fddb3854d78d6d00afee5019758b91e38278
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 14:34:42 +0900

    [day8-part4] generic

---
+++ day8-part4-collection/day8-part4-collection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day8_part4_collection
+{
+    class Program
+    {
+        // 1. Generic (accepts all types)
+        class Cup<T>
+        {
+            public T Content { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Cup<string> cupOfString = new Cup<string> { Content = "Coffee" };
+            Cup<int> cupOfInt = new Cup<int> { Content = 42 };
+
+            Console.WriteLine($"CupOfString: {cupOfString.Content}");
+            Console.WriteLine($"cupOfInt: {cupOfInt.Content}");
+        }
+    }
+}
---
+++ day8-part4-collection/day8-part4-collection/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("day8-part4-collection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("day8-part4-collection")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32ea554c-5dc9-4524-be1a-7199d17ff9c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
