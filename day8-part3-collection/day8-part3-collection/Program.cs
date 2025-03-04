9771c4803a8f29539175692d838d696e892a7e82
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 13:27:38 +0900

    [day8-part3] list example

--- day8-part3-collection/day8-part3-collection/Program.cs
+++ day8-part3-collection/day8-part3-collection/Program.cs
@@ -18,15 +18,34 @@ namespace day8_part3_collection
             //    Console.WriteLine(num);
             //}
 
-            // 1. list (unlike array which is fixed, it can change size dynamically)
-            List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
+            //// 1. list (unlike array which is fixed, it can change size dynamically)
+            //List<string> names = new List<string> { "Alice", "Bob", "Charlie" };
 
-            names.Add("Dave"); // add name
-            names.Remove("Bob"); // remove name
+            //names.Add("Dave"); // add name
+            //names.Remove("Bob"); // remove name
 
-            foreach(var name in names)
+            //foreach(var name in names)
+            //{
+            //    Console.WriteLine(name); // new list for names
+            //}
+
+            // Example
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            Console.WriteLine(list[1]);
+            list.Insert(1, 100); // add value and move numbers
+            Console.WriteLine(list[1]);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list.Count);
+
+            foreach (int i in list)
             {
-                Console.WriteLine(name); // new list for names
+                Console.WriteLine(i);
             }
         }
     }
