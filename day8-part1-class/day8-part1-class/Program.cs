39572f54a20a918983aaf4c6cc85af50c5de206b
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-04 09:34:42 +0900

    [day8] stopwatch

--- day8-part1-class/day8-part1-class/Program.cs
+++ day8-part1-class/day8-part1-class/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace day8_part1_class
@@ -18,11 +20,23 @@ namespace day8_part1_class
 
             //Environment.Exit(0); // program ends automatically
 
-            // 2. Random
-            Random random = new Random();
+            //// 2. Random
+            //Random random = new Random();
 
-            int randomNumber = random.Next(1, 101);
-            Console.WriteLine("Random Number: " + randomNumber);
+            //int randomNumber = random.Next(1, 101);
+            //Console.WriteLine("Random Number: " + randomNumber);
+
+            // 3. Saving Runtime
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1); // 0.001
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}ms");
         }
     }
 }
