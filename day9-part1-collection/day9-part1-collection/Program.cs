c818b54fe45394c3e5cd825847aec87e8ee17ee1
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 09:20:09 +0900

    [day9-part1] lambda function

---
+++ day9-part1-collection/day9-part1-collection/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part1_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. lambda function
+            string[] names = { "Charlie", "Alice", "Bob" };
+            var sortedNames = names.OrderBy(n => n);
+
+            foreach (var name in sortedNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            var firstName = names.First(n => n.StartsWith("A"));
+            Console.WriteLine($"First name starting with A: {firstName}");
+        }
+    }
+}
