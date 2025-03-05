6c96113c100aa1f8d61d32c498cecd82cf9ac77a
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 10:19:15 +0900

    [day9-part3] algorithm - count

--- day9-part2-algorithm/day9-part2-algorithm/Program.cs
+++ day9-part2-algorithm/day9-part2-algorithm/Program.cs
@@ -10,13 +10,22 @@ namespace day9_part2_algorithm
     {
         static void Main(string[] args)
         {
-            // 1. sum
-            int[] data = { 1, 2, 3, 4, 5 };
-            int sum = 0;
+            //// 1. sum
+            //int[] data = { 1, 2, 3, 4, 5 };
+            //int sum = 0;
 
-            foreach (var d in data) sum += d;
+            //foreach (var d in data) sum += d;
 
-            Console.WriteLine($"Sum: {sum}");
+            //Console.WriteLine($"Sum: {sum}");
+
+            // ----------------------------------------------------------------------------------------------------
+
+            // 2. count
+            int[] data = { 2, 4, 6, 8, 10 };
+
+            int count = data.Length; // finding the length
+
+            Console.WriteLine($"Count: {count}");
         }
     }
 }
