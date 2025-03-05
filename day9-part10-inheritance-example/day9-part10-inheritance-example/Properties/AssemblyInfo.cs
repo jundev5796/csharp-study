713b1619dd8ac1fe453ed583756984b1404f97ed
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 15:18:32 +0900

    [day9-part10] inheritance example

---
+++ day9-part10-inheritance-example/day9-part10-inheritance-example/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("day9-part10-inheritance-example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("day9-part10-inheritance-example")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c536d2b8-0a20-49e2-bbfd-422e15d46299")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ day9_part11_example2/day9_part11_example2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part11_example2
+{
+    class Skill
+    {
+        public string Name; // skill name
+        public int ManaCost; // mana usage
+        public int Cooldown;
+        public int LastUsedTime;
+
+        public Skill(string name, int manaCost, int cooldown)
+        {
+            Name = name;
+            ManaCost = manaCost;
+            Cooldown = cooldown * 100;
+            LastUsedTime = 0;
+        }
+
+        // 스킬 사용 가능 여부 확인
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
