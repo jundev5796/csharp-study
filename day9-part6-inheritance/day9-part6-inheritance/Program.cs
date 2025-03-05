a865edcee8812da2a0053035ec070d6273c91ef5
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 13:29:37 +0900

    [day9-part6] inheritance - upcasting

---
+++ day9-part6-inheritance/day9-part6-inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part6_inheritance
+{
+    // upcasting
+    // convert child -> parent
+    // convert without data loss
+
+    class Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Makes animal sound.");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog(); // child
+            Animal myAnimal = myDog; // upscaling (child -> parent)
+
+            myAnimal.Speak(); // valid code
+        }
+    }
+};
