d8d0a5478db94e0338910e7127e6898f6aec8e75
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 11:51:01 +0900

    [day9-part4] inheritance (how it works)

---
+++ day9-part4-inheritance/day9-part4-inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part4_inheritance
+{
+    class Player
+    {
+        public string name;
+
+        public void Render()
+        {
+            Console.WriteLine("Player: " + name);
+        }
+    }
+
+    class Wizard : Player
+    {
+        public string job;
+
+        public void Render2()
+        {
+            Console.WriteLine("Job: " + job);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player(); // instance
+
+            p.name = "Lion";
+            p.Render();
+
+
+            Wizard w = new Wizard();
+            w.name = "Merlin";
+            w.job = "Wizard";
+            w.Render();
+            w.Render2();
+        }
+    }
+}
