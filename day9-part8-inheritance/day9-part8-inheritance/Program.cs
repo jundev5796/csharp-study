689cdd2e0580cb605df6a5c6afc57d80367b8de6
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 14:05:43 +0900

    [day9-part8] inheritance - base

---
+++ day9-part8-inheritance/day9-part8-inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part8_inheritance
+{
+    // call base parent
+    class Parent
+    {
+        public virtual void ShowMessage()
+        {
+            Console.WriteLine("parent class message");
+        }
+    }
+
+    class Child : Parent
+    {
+        public override void ShowMessage()
+        {
+            Console.WriteLine("child class message");
+            base.ShowMessage();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+            child.ShowMessage();
+        }
+    }
+}
