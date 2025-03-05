9031e10ddf0e3ad9e5bcdfc2b74af7173f76de3c
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-05 14:27:58 +0900

    [day9-part9] inheritance - protected variables

---
+++ day9-part9-inheritance/day9-part9-inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day9_part9_inheritance
+{
+    class Player
+    {
+        protected string Name;
+
+        public Player()
+        {
+            Name = "Player";
+            Console.WriteLine("parent instance.");
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Wizard : Player
+    {
+        public Wizard()
+        {
+            Name = "Wizard";
+            Console.WriteLine("child instance");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p = new Player();
+            p.Show();
+
+            Wizard w = new Wizard();
+            w.Show();
+        }
+    }
+}
