148f7433b6fda88dfabc82676bb8ccdaadd88db0
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 09:47:12 +0900

    [day10-part3-review] constructor w/ parameters

--- day10-part3/day10-part3/Program.cs
+++ day10-part3/day10-part3/Program.cs
@@ -11,17 +11,36 @@ namespace day10_part3
         class Person
         {
             public string Name;
+            public int Age;
 
             public Person() // constructor
             {
                 Name = "Unknown";
+                Age = 0;
+            }
+
+            public Person(string name)
+            {
+                Name = name;
+                Age = 0;
+            }
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
             }
         }
 
         static void Main(string[] args)
         {
-            Person p = new Person();
-            Console.WriteLine(p.Name);
+            Person p = new Person(); // instance
+            Person p1 = new Person("Bob");// instance with 1 parameter
+            Person p2 = new Person("카타리나", 20); // instance with 2 parameter
+
+            Console.WriteLine(p.Name + "," + p.Age);
+            Console.WriteLine(p1.Name + "," + p1.Age);
+            Console.WriteLine(p2.Name + "," + p2.Age);
         }
     }
 }
