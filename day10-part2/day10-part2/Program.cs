b997e06cf14409d57f21a4f3592d51504e8e71bf
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 09:26:19 +0900

    [day10-part2] class field

---
+++ day10-part2/day10-part2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10_part2
+{
+    class Person
+    {
+        public string name; // field: a space that contains class data
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person(); // instance
+            p.name = "Alice"; // data inside field
+            Console.WriteLine(p.name);
+        }
+    }
+}
