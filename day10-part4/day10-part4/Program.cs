deb3989afd7d1f188cca3ad637a26e42b72ef046
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 10:06:48 +0900

    [day10-part4] destructor

---
+++ day10-part4/day10-part4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10_part4
+{
+    class MyResource
+    {
+        // destructor (소멸자)
+        ~MyResource()
+        {
+            Console.WriteLine("삭제될때 호출");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GC(Garbage Collector)에 의해 나중에 소멸자 호출
+            // 가비지 컬렉션은 더이상 참조되지 않는 객체를 정리함
+            MyResource r = new MyResource();
+        }
+    }
+}
---
+++ day10-part4/day10-part4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("day10-part4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("day10-part4")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a9d1d2ba-6bea-460e-8614-ad0c09635b73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
