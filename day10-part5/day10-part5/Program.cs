06569f2f116b8918f7ad7d45ad0efc1c16b723c0
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 10:17:01 +0900

    [day10-part5] ref pointer

---
+++ day10-part5/day10-part5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10_part5
+{
+    class Program
+    {
+        // ref pointer concept
+        // method ref/out
+        static void Increase(ref int x)
+        {
+            x++;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Increase(ref a);
+
+            Console.WriteLine("Value A: " + a); // value connects without return
+        }
+    }
+}
