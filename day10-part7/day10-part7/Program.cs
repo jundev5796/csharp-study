d56363607fb341aeb254f26c9914debd6eb1ab4b
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 10:53:22 +0900

    [day10-part7] base constructor (:base)

---
+++ day10-part7/day10-part7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10_part7
+{
+    class Parent
+    {
+        public Parent(string message)
+        {
+            Console.WriteLine("Parent Constructor: " + message); // 2
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child():base("Success!") // call parent constructor // 1
+        {
+            Console.WriteLine("called child constructor"); // 3
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();
+        }
+    }
+}
