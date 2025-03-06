3aef1218fbe10fefd5d1fd48eaac46f034abfeb4
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 09:22:19 +0900

    [day10-part1] connecting namespace

---
+++ day10-part1/day10-part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourNamespace; // create shortcut
+
+// new namespace
+namespace YourNamespace
+{
+    class MyClass
+    {
+        public void Greet() => Console.WriteLine("Greetings!");
+    }
+}
+
+
+namespace day10_part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. connecting another namespace
+            MyClass myClass = new MyClass();
+            myClass.Greet();
+        }
+    }
+}
