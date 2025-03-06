7394fd0b8b8f29ad064dfb086fa395a46c44cbb4
hyunjunbaek <hjbaek5796@gmail.com>
2025-03-06 15:28:35 +0900

    [day10-part12] interface example

---
+++ day10-part12/day10-part12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day10_part12
+{
+    // 인터페이스를 활용하면 객체가 어떤 특정한 동작을 보장하면서도 다양한 형태로 사용
+    // 여러 동물을 공통된 방식으로 다룰수있다
+
+    // 인터페이스 정의
+    interface IAnimal
+    {
+        void Speak();
+    }
+
+    class Dog : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+
+    class Cat : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+
+    class AnimalTrainer
+    {
+        public void Train(IAnimal animal)
+        {
+            Console.WriteLine("Animal makes a sound.");
+            animal.Speak();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalTrainer trainer = new AnimalTrainer();
+
+            IAnimal myDog = new Dog();
+            IAnimal myCat = new Cat();
+
+            trainer.Train(myDog);
+            trainer.Train(myCat);
+        }
+    }
+}
